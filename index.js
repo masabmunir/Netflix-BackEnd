b7bd9e23fe0b468c071473e55748ca5a9916bda0
alishba2 <alishbashazadi013@gmail.com>
2022-10-25 19:09:59 +0500

    gridfs

--- index.js
+++ index.js
@@ -11,15 +11,35 @@ const imageRoutes = require('./routes/images.routes');
 // Movies Path
 const movieModal = require('./dataModals/movies.modal');
 const movieRoutes = require('./routes/movies.routes');
+const storage = require('./storage');
+
+
+
+  
+  //to parse json content
+  app.use(express.json());
+  //to parse body from url
+  app.use(express.urlencoded({
+    extended: false
+  }));
+  
 
 // For uploading large images/videos
 
-app.use(express.json({limit: "50mb", extended: true}))
-app.use(express.urlencoded({limit: "50mb", extended: true, parameterLimit: 500000}))
+app.use(bodyParser.json({
+   limit: '50mb'
+ }));
+ 
+ app.use(bodyParser.urlencoded({
+   limit: '50mb',
+   parameterLimit: 100000,
+   extended: true 
+ }));
 
 
 const videoModel = require('./routes/videoroute');
 const songModal = require('./routes/sound.routes');
+const { db } = require('./dataModals/images.modal');
 
 
 const port = process.env.port || 8000
---
+++ storage.js
@@ -0,0 +1,66 @@
+// var express = require('express');
+// const multer = require("multer");
+// const mongoose = require('./db/conn');
+// const router = express.Router();
+// const movModal = require('./dataModals/movies.modal')
+// const ObjectID = require('mongoose').Types.ObjectId;
+// const { delUser, getMovies, singleMovies, addMovies, updateMovies, delMovies } = require('../Controllers/movie-controller');
+
+// const {
+//   GridFsStorage
+// } = require("multer-gridfs-storage");
+
+// require("dotenv")
+//   .config();
+
+// //   Bucket
+//   let bucket;
+//   mongoose.connection.on("connected", () => {
+//     var db = mongoose.connections[0].db;
+//     bucket = new mongoose.mongo.GridFSBucket(db, {
+//       bucketName: "newBucket"
+//     });
+//     console.log(bucket);
+//   });
+
+// //   Storage
+//   storage = new GridFsStorage({
+//    url: 'mongodb://localhost:27017/websitework',
+//    file: (req, file) => {
+//      return new Promise((resolve, reject) => {
+//        const filename = file.originalname;
+//        const fileInfo = {
+//          filename: filename,
+//          bucketName: "newBucket"
+//        };
+//        resolve(fileInfo);
+//      });
+//    }
+//  });
+ 
+// upload = multer({storage});
+
+// router.get('/', getMovies)
+
+// // Getting ID of Single Movie
+
+// router.get('/:id', singleMovies);
+
+// // Post Request 
+// router.post('/addMovie',upload.single('file'), addMovies, {
+
+   
+   
+    
+        
+// });
+// //Update Route
+
+// router.put('/:id',updateMovies);
+
+// // Delete Route 
+
+// router.delete('/:id',delMovies);
+
+
+// module.exports = router;
